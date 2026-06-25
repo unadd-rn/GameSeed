6d35c7bcf4cafab977953552bf7b8d279c46fc18
unadd-rn <auliaunad@gmail.com>
2026-06-26 01:13:35 +0700

    cleanup

--- GameSeed/Assets/Scripts/BodyTypeDefault.cs
+++ GameSeed/Assets/Scripts/BodyTypeDefault.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DefaultStick", menuName = "Body Type/Default")]
 public class BodyTypeDefault : BodyType
 {
-    // Inherits all fields from BodyType automatically.
-    // You can add logic or unique stats specific to the default body type here if needed.
+ 
 }
