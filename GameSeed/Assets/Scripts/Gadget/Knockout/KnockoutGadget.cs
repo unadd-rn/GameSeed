32cb6ef1a02ce047a5a56b2eb23740ce22067521
unadd-rn <auliaunad@gmail.com>
2026-06-27 02:56:23 +0700

    radial menu

--- GameSeed/Assets/Scripts/Gadget/Knockout/KnockoutGadget.cs
+++ GameSeed/Assets/Scripts/Gadget/Knockout/KnockoutGadget.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-[CreateAssetMenu(menuName = "Gadgets/KnockoutGadget")]
+// [CreateAssetMenu(menuName = "Gadgets/KnockoutGadget")]
 
-public class KnockoutGadget : BaseGadget
-{
-    public override void Apply(StickData target)
-    {
-        target.canActivateSafeArea++;
-    }
+// public class KnockoutGadget : BaseGadget
+// {
+//     public override void Apply(StickData target)
+//     {
+//         target.canActivateSafeArea++;
+//     }
 
-    public override void Remove(StickData target)
-    {
-        // Mengurangi kuota saat gadget dilepas
-        if (target.canActivateSafeArea > 0) 
-            target.canActivateSafeArea--;
-        else 
-            Debug.LogWarning("Tidak ada kuota Safe Area yang bisa dikurangi.");
-    }
-}
+//     public override void Remove(StickData target)
+//     {
+//         // Mengurangi kuota saat gadget dilepas
+//         if (target.canActivateSafeArea > 0) 
+//             target.canActivateSafeArea--;
+//         else 
+//             Debug.LogWarning("Tidak ada kuota Safe Area yang bisa dikurangi.");
+//     }
+// }
